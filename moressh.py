94e6c7c18c1ffb0e44869eae8a5afd28b63024c5
q629988171 <113960096@qq.com>
2022-02-09 20:55:53 +0800

    修复无法执行多个命令的问题
    非Linux主机不支持连接符: ";" 比如: Windows, 交换机等设备

--- moressh.py
+++ moressh.py
@@ -44,8 +44,51 @@ def get_hosts(data):
     return hosts
 
 
-async def ssh2(host_conf):
-    """连接SSH, 执行命令
+async def ssh_connect(host, port, username, password, known_hosts):
+    """连接SSH
+
+    :param host: 主机
+    :param port: 端口
+    :param username: 用户名
+    :param password: 密码
+    :param known_hosts: 访问过计算机的公钥
+    :return: 连接对象
+    """
+    conn = await asyncssh.connect(host=str(host), port=port, username=str(username), password=str(password),
+                                  known_hosts=known_hosts)
+    return conn
+
+
+async def run_commands(host, port, username, password, known_hosts, commands):
+    """执行多个命令
+
+    :param host: 主机
+    :param port: 端口
+    :param username: 用户名
+    :param password: 密码
+    :param known_hosts: 访问过计算机的公钥
+    :param commands: 命令
+    :return: 执行结果
+    """
+    results = []
+    try:
+        conn = await ssh_connect(host=str(host), port=port, username=str(username), password=str(password),
+                                 known_hosts=known_hosts)
+        async with conn:
+            # 执行SSH命令
+            for command in commands.split(';'):
+                result = await conn.run(command, check=True)
+                results.append(result.stdout)
+            return 0, results
+
+    except Exception as e:
+        message = format_message(e.args)
+        errcode, errmsg = message[0], message[1]
+        return errcode, errmsg
+
+
+async def run_command(host_conf):
+    """执行命令
 
     :param host_conf: 主机信息(字典)
     """
@@ -55,34 +98,20 @@ async def ssh2(host_conf):
     password = host_conf.get('password', 'admin')
     # 如果机器时第一次SSH登录，需要将known_hosts 设置为None，否在会报错
     known_hosts = host_conf.get('known_hosts', None)
-    command = host_conf.get('command', 'whoami')
-    logger.info(f"正在连接: {host}")
-    # 初始化一个SSH连接
-    try:
-        async with asyncssh.connect(
-                host=str(host),
-                port=port,
-                username=str(username),
-                password=str(password),
-                known_hosts=known_hosts
-        ) as conn:
-            # 执行SSH命令
-            result = await conn.run(command)
-            logger.info(f"连接成功: {host}, 返回结果: {result.stdout.strip()}")
-    except Exception as e:
-        message = format_message(e.args)
-        errcode, errmsg = message[0], message[1]
-        logger.info(f"连接失败: {host}, errcode: {errcode}, errmsg: {errmsg}")
+    commands = host_conf.get('commands', 'whoami')
+    logger.info(f"正在连接: {host}:{port}")
+    result = await run_commands(host, port, username, password, known_hosts, commands)
+    logger.info(f"连接完成: {host}:{port}, errcode: {result[0]}, errmsg: {result[1]}")
 
 
-async def excutor(hosts):
-    """调度SSH2协程
+async def parallel_run(hosts):
+    """连接多个主机
 
     :param hosts: 主机信息(字典)
     """
     tasks = []
     for host in hosts:
-        task = asyncio.create_task(ssh2(host))
+        task = asyncio.create_task(run_command(host))
         tasks.append(task)
     for f in tqdm(asyncio.as_completed(tasks), total=len(tasks), desc="Connect SSH"):
         await f
@@ -90,7 +119,7 @@ async def excutor(hosts):
 
 def main():
     # 移除控制台输出
-    logger.remove(handler_id=None)
+    # logger.remove(handler_id=None)
     logger.add(sink="logs/moressh_{time}.log", enqueue=True)
     filepath = f"device_list.csv"
     logger.info(f"正在加载: {filepath}")
@@ -100,7 +129,7 @@ def main():
     print('Starting, use <Ctrl-C> to stop')
     loop = asyncio.get_event_loop()
     try:
-        loop.run_until_complete(excutor(hosts))
+        loop.run_until_complete(parallel_run(hosts))
     except KeyboardInterrupt:
         asyncio.gather(*asyncio.Task.all_tasks()).cancel()
         loop.stop()
